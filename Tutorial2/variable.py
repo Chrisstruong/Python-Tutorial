f55c9c4503b214b47938e3470a94ee748d2bd699
chrisstruong <chrisstruong@gmail.com>
2023-04-12 12:31:48 -0400

    Finish python variable

---
+++ Tutorial2/variable.py
@@ -0,0 +1,60 @@
+# Variable = a resuable container for stroing a value
+#            a variable behaves as it were the value it contains
+
+# INTEGER
+
+age = 21
+players = 2
+quantity = 5
+
+# print(f"You are {age} years old")
+# print(f"There are {players} players online")
+# print(f"You would like to buy ${quantity} items")
+
+# FLOAT
+
+gpa = 3.2
+distance = 2.5
+price = 10.99
+
+# print(f"Your GPA is {gpa}")
+# print(f"You ran {distance}Km")
+# print(f"The price is ${price}")
+
+# STRING
+
+name = "Bro"
+food = "pizza"
+email = "Bro123@gmail.com"
+
+# print(f"Hello {name}")
+# print(f"You like {food}")
+# print(f"Your email is: {email}")
+
+
+# BOOLEAN
+online = True
+for_sale = False
+running = True
+
+# print(f"Are you online?: {online}")
+# print(f"Is the item for sale?: {for_sale}")
+# print(f"Game running: {running}")
+#
+# if (running):
+#     print("The game is running")
+# else:
+#     print(f"The game is running")
+
+x = y = z = 1
+print(x,y,z)
+print(y)
+print(z)
+
+
+
+# age = 21
+#
+# print("You are " + str(age) + " years old")
+# print("You are", age, "years old")
+# print(f"You are {age} years old")
